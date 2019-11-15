651de04fc1bde18749e18a4c8a1c0ed1c4f7f2b9
SHUHEI HIYA <syuhei176@gmail.com>
2019-11-16 00:28:37 +0900

    change template ext

--- src/index.js
+++ src/index.js
@@ -1,112 +1,108 @@
-const CodeMirror = require("codemirror");
-require("codemirror/mode/javascript/javascript");
-require("codemirror/mode/python/python");
-require("codemirror/lib/codemirror.css");
-require("codemirror/theme/yonce.css");
+const CodeMirror = require('codemirror')
+require('codemirror/mode/javascript/javascript')
+require('codemirror/mode/python/python')
+require('codemirror/lib/codemirror.css')
+require('codemirror/theme/yonce.css')
 
-const pegSrc = require("../lib/chamber.txt");
-const solTemplate = require("../lib/sol.txt");
-const OVMCompiler = require("../lib");
+const pegSrc = require('../lib/chamber.txt')
+const solTemplate = require('../lib/sol.ejs')
+const OVMCompiler = require('../lib')
 
-const checkpoint = require("../examples/checkpoint.txt");
-const exit = require("../examples/exit.txt");
-const ownership = require("../examples/ownership.txt");
-const swap = require("../examples/swap.txt");
-const order = require("../examples/order.txt");
+const checkpoint = require('../examples/checkpoint.txt')
+const exit = require('../examples/exit.txt')
+const ownership = require('../examples/ownership.txt')
+const swap = require('../examples/swap.txt')
+const order = require('../examples/order.txt')
 const examples = {
   checkpoint,
   exit,
   ownership,
   swap,
   order
-};
+}
 
 function main() {
-  const codearea = document.getElementById("codearea");
-  const solidity = document.getElementById("solidity");
-  const messageDom = document.getElementById("message");
+  const codearea = document.getElementById('codearea')
+  const solidity = document.getElementById('solidity')
+  const messageDom = document.getElementById('message')
 
-  codearea.textContent = examples.checkpoint;
+  codearea.textContent = examples.checkpoint
   const inputArea = CodeMirror.fromTextArea(codearea, {
     lineNumbers: true,
-    theme: "yonce",
-    mode: "python"
-  });
+    theme: 'yonce',
+    mode: 'python'
+  })
   const outputArea = CodeMirror.fromTextArea(solidity, {
     lineNumbers: true,
-    theme: "yonce",
+    theme: 'yonce',
     readOnly: true,
-    mode: "javascript"
-  });
-  inputArea.on("change", function(instance) {
+    mode: 'javascript'
+  })
+  inputArea.on('change', function(instance) {
     try {
-      compile(instance);
+      compile(instance)
     } catch (e) {
-      console.error(e);
+      console.error(e)
       messageDom.innerText =
-        "parse error at line " +
+        'parse error at line ' +
         e.location.start.line +
-        " column " +
-        e.location.start.column;
-      messageDom.className = "error";
+        ' column ' +
+        e.location.start.column
+      messageDom.className = 'error'
     }
-  });
-  compile(inputArea);
+  })
+  compile(inputArea)
 
-  delegate(document.querySelector(".btns"), ".example", "click", e => {
-    const exampleName = e.target.id;
-    inputArea.setValue(examples[exampleName]);
-  });
+  delegate(document.querySelector('.btns'), '.example', 'click', e => {
+    const exampleName = e.target.id
+    inputArea.setValue(examples[exampleName])
+  })
 
   function compile(instance) {
-    const result = OVMCompiler.compile(
-      instance.getValue(),
-      solTemplate,
-      pegSrc
-    );
-    outputArea.setValue(result);
-    messageDom.innerText = "succeed";
-    messageDom.className = "success";
+    const result = OVMCompiler.compile(instance.getValue(), solTemplate, pegSrc)
+    outputArea.setValue(result)
+    messageDom.innerText = 'succeed'
+    messageDom.className = 'success'
   }
 }
 
-main();
+main()
 
 /* Utils */
 
 function on(target, type, callback, useCapture) {
   if (target.addEventListener) {
-    target.addEventListener(type, callback, !!useCapture);
+    target.addEventListener(type, callback, !!useCapture)
   } else {
-    target.attachEvent("on" + type, function() {
-      callback.call(target);
-    });
+    target.attachEvent('on' + type, function() {
+      callback.call(target)
+    })
   }
 }
 
 function delegate(target, selector, type, handler) {
   function dispatchEvent(event) {
-    var targetElement = event.target;
-    var potentialElements = qsa(selector, target);
+    var targetElement = event.target
+    var potentialElements = qsa(selector, target)
     var hasMatch =
-      Array.prototype.indexOf.call(potentialElements, targetElement) >= 0;
+      Array.prototype.indexOf.call(potentialElements, targetElement) >= 0
     var hasMatchParent =
       Array.prototype.indexOf.call(
         potentialElements,
         targetElement.parentElement
-      ) >= 0;
+      ) >= 0
 
     if (hasMatch) {
-      handler.call(targetElement, event);
+      handler.call(targetElement, event)
     } else if (hasMatchParent) {
-      handler.call(targetElement.parentElement, event);
+      handler.call(targetElement.parentElement, event)
     }
   }
 
-  var useCapture = type === "blur" || type === "focus";
-  on(target, type, dispatchEvent, useCapture);
+  var useCapture = type === 'blur' || type === 'focus'
+  on(target, type, dispatchEvent, useCapture)
 }
 
 function qsa(selector, scope) {
-  return (scope || document).querySelectorAll(selector);
+  return (scope || document).querySelectorAll(selector)
 }
